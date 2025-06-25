c5702c2772ac199764fa3c3681819050c3233022
Petro <larrypetroski@gmail.com>
2025-06-25 16:26:16 -0500

    removing firebase config values

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -13,13 +13,7 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideFirebaseApp(() =>
       initializeApp({
-        apiKey: 'AIzaSyA6LZSrLu-u5CAiWgEpmORIxu-pOoyyGv0',
-        authDomain: 'clips-v2-fbfa2.firebaseapp.com',
-        projectId: 'clips-v2-fbfa2',
-        storageBucket: 'clips-v2-fbfa2.firebasestorage.app',
-        messagingSenderId: '1015204087006',
-        appId: '1:1015204087006:web:726a2feca629e5e5d3ae4f',
-        measurementId: 'G-T4WELNL19J',
+        
       })
     ),
     provideAuth(() => getAuth()),
